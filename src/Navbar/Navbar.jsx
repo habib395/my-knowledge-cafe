1d4a27822255c55c6d71d93999d15b7f8360d47b
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-11-28 22:34:27 +0600

    new project start in react

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+        <div className="text-2xl font-semibold flex justify-between items-center w-10/12 mx-auto">
+            <h2>Knowledge Cafe</h2>
+            <p><img style={{width: "50px"}} src="../../images/profile.png" alt="" /></p>
+        </div>
+    );
+};
+
+export default Navbar;
